0e83beb4c3f74e43496b72890769d173e09e3b5a
Vaughn Wilcox <vaughn_wilcox@yahoo.com>
2024-02-05 18:34:08 -0500

    Initial front end push

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello Aaron and Nick, how's it going?")
---
+++ src/components/auth/login/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useRef } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { baseURL } from '../../../environment';
+import { useNavigate } from 'react-router-dom';
+
+function Login({updateToken}) {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // console.log(emailRef)
+        // console.log(passwordRef)
+
+        const body = JSON.stringify({
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        })
+
+        const url = `${baseURL}/user/login`;
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                }),
+                body: body
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            updateToken(data.token)
+
+            if(data.message === 'Login Successful') {
+                updateToken(data.token)
+                navigate('/tasks')
+            } else {
+                alert(data.message)
+            }
+
+        } catch (err) {
+            console.error(err.message)
+        }    }
+
+  return (
+    <>
+    <h2>Log In</h2>
+    <Form onSubmit={handleSubmit}>
+        <FormGroup>
+            <Label>E-mail:</Label>
+            <Input 
+                placeholder='Your E-mail'
+                type='email'
+                innerRef={emailRef}
+                autoComplete={'off'}
+            />
+        </FormGroup>
+        <FormGroup>
+            <Label>Password:</Label>
+            <Input 
+                placeholder='Password'
+                type='password'
+                innerRef={passwordRef}
+                autoComplete={'off'}
+            />
+        </FormGroup>
+            <Button type='submit'>Log In</Button>
+    </Form>
+    </>
+  )
+}
+
+export default Login
