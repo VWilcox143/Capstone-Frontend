ce99e5614d3017e2a20cd54abf06cd5012fe778d
Vaughn Wilcox <vaughn_wilcox@yahoo.com>
2024-02-18 16:20:49 -0500

    Fixed TaskEdit, Receipt Create working, added some OnClick to ttaskTable, created TaskDetail component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 import Auth from './components/auth/Auth';
 import { useState, useEffect  } from 'react';
 import TaskIndex from './components/tasks/TaskIndex'
-// import ReceiptCreate from './components/receipts/'
 import Logout from './components/auth/login/Logout';
-import ReceiptCreate from './components/receipts/ReceiptCreate';
-
+import TaskEdit from './components/tasks/TaskEdit';
+import TaskDetail from './components/tasks/TaskDetail';
+import ReceiptIndex from './components/receipts/ReceiptIndex';
 
 
 function App() {
@@ -43,16 +43,16 @@ function App() {
           element={<TaskIndex token={sessionToken}/>}
         />
         <Route 
-          path='/tasks/update/:id'
-          element={<h1>Hello world</h1>}
-        />
-        {/* <Route 
           path='/receipts/add/:id'
-          element={<ReceiptCreate token={sessionToken}/>}
-        /> */}
+          element={<ReceiptIndex token={sessionToken}/>}
+        /> 
         <Route
         path='/tasks/update/:id'
         element={<TaskEdit token={sessionToken} />}
+        />
+        <Route
+        path='/tasks/task/:id'
+        element={<TaskDetail token={sessionToken} />}
         />
       </Routes>
     </div>
---
+++ src/components/receipts/ReceiptIndex.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import { baseURL } from '../../environment';
+import { Container, Col, Row } from 'reactstrap';
+import ReceiptCreate from './ReceiptCreate';
+import ReceiptsTable from './ReceiptsTable'
+import { useParams } from 'react-router-dom';
+
+function ReceiptIndex(props) {
+
+const { id } = useParams();
+
+const [ receipt, setReceipt] = useState([]);
+console.log(receipt)
+const fetchReceipts = async () => {
+    const url = `${baseURL}/receipt/${id}`
+
+    const requestOptions = {
+        method:'GET',
+        headers: new Headers ({
+            "Authorization": props.token
+        })
+    }
+
+    try {
+        const rest = await fetch (url, requestOptions);
+        const data = await rest.json();
+        setReceipt(data.result)
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+useEffect(() => {
+    if(props.token) {
+        fetchReceipts();
+    }
+}, [props.token])
+
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col md='4'>
+                <ReceiptCreate 
+                token = {props.token}
+                fetchReceipts= {fetchReceipts} />
+            </Col>
+            <Col md='8'>
+                <ReceiptsTable
+                token= {props.token}
+                fetchReceipt= {fetchReceipts}
+                receipts={receipt} />
+            </Col>
+        </Row>
+    </Container>
+    </>
+  )
+}
+
+export default ReceiptIndex
