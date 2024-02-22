5cb3991d29009adc2bd208beea239214afed1548
Nick <nkrose117@gmail.com>
2024-02-21 20:10:21 -0500

    updated

--- src/components/receipts/ReceiptEdit.jsx
+++ src/components/receipts/ReceiptEdit.jsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState} from 'react'
-import { Form, useNavigate, useNavigate, useParams } from 'react-router-dom'
-import { baseURL, baseURL } from '../../environment';
+import { Form, useNavigate, useParams } from 'react-router-dom'
+import { baseURL } from '../../environment';
 import { Button, Col, Container, FormGroup, Input, Label, Row, Table } from 'reactstrap';
 import FullButtons from '../buttons/FullButtons';
-import React from "react";
-import React from "react";
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { Button, Table } from "reactstrap";
-import { Button, Table } from "reactstrap";
-import { Button, Table } from "reactstrap";
-import { baseURL } from "../../environment";
-import { baseURL } from "../../environment";
-import { baseURL } from "../../environment";
 
 export default function ReceiptEdit(props) {
 
@@ -191,77 +179,4 @@ export default function ReceiptEdit(props) {
         </Container>
     </>
     )
-}
-export function ReceiptsTable({ tasks, receipts, token, fetchReceipts }) {
-
-    const navigate = useNavigate();
-
-    async function deleteReceipts(id) {
-        const url = `${baseURL}/receipt/${id}`;
-
-        let requestOptions = {
-            headers: new Headers({
-                'Authorization': token
-            }),
-            method: 'DELETE'
-        };
-        try {
-
-            let response = await fetch(url, requestOptions);
-            let data = await response.json();
-
-            if (data) {
-                fetchReceipts();
-            }
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-
-    return (
-        <>
-            <h1>Receipts</h1>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>
-                            Type
-                        </th>
-                        <th>
-                            Date
-                        </th>
-                        <th>
-                            Amount
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {typeof receipts === "object" &&
-                        receipts.map(receipts => (
-                            <tr key={receipts._id}>
-                                <th scope='row'>{receipts.type}</th>
-                                <td>{receipts.date}</td>
-                                <td>{receipts.amount}</td>
-                                <td>
-                                    <Button
-                                        onClick={() => deleteReceipts(receipts._id)}
-                                        color='danger'
-                                    >Delete</Button>
-                                </td>
-                                <td>
-                                    <Button
-                                        onClick={(event) => {
-                                            event.stopPropagation();
-                                            navigate(`/tasks/update/${receipts._id}`);
-                                        } }
-                                        color='warning'
-                                    >Edit</Button>
-                                </td>
-                            </tr>
-                        ))}
-                </tbody>
-            </Table>
-        </>
-    );
 }
